e4ab2021ff2ef29d4f96958a5c0a486a2e0a81b5
shamy <shamyrquishpet@gmail.com>
2024-10-25 14:37:47 -0500

    datepickers

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular'
+import * as moment from 'moment';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  display: string = '';
+  fechaini!: string;
+  fechafin!: string;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  append(value: string){
+    this.display += value;
+  }
+
+  calcular(){
+      let ini = moment(this.fechaini);
+      let fin = moment(this.fechafin);
+      let diff = fin.diff(ini, 'days');
+      this.display = diff.toString();
+      console.log(diff);
+  }
 
 }
