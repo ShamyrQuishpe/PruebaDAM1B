e09a6f7a809a730b0457cc2f4902f010b10c630a
shamy <shamyrquishpet@gmail.com>
2024-10-25 15:03:14 -0500

    arreglo

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Directory, Filesystem } from '@capacitor/filesystem';
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss']
 })
+
 export class Tab4Page {
 
   constructor() {}
-
+  
 }
