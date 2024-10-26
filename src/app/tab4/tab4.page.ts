3f606c6ff2da42917f6965d7441c3f82eb4af1dc
shamy <shamyrquishpet@gmail.com>
2024-10-25 23:17:05 -0500

    raices

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Directory, Filesystem } from '@capacitor/filesystem';
+
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
@@ -8,6 +8,35 @@ import { Directory, Filesystem } from '@capacitor/filesystem';
 
 export class Tab4Page {
 
-  constructor() {}
+  a: number = 0;
+  b: number = 0;
+  c: number = 0;
+  result: string = '';
+
+  onSubmit(){
+    this.result = this.calculate(this.a,this.b,this.c);
+  }
+
+  calculate(a:number, b:number, c:number):string{
+    
+    if(a===0){
+      return "El valor de a no puede ser 0"
+    }
+    const funcion = b*b-4*a*c;
+
+    if(funcion>0){
+      const raiz1 = (-b + Math.sqrt(funcion)) / (2 * a);
+      const raiz2 = (-b - Math.sqrt(funcion)) / (2 * a); 
+      return `Raíces reales: ${raiz1} y ${raiz2}`;
+    }else if(funcion===0){
+      const raiz = -b/(2*a)
+      return `Raíz doble: ${raiz}`
+    }else{
+      const real = (-b / (2 * a)).toFixed(2);
+      const imaginario = (Math.sqrt(-funcion) / (2 * a)).toFixed(2);
+      return `Raíces complejas: ${real} ± ${imaginario}i`;
+    }
+
+  }
   
 }
---
+++ src/app/tab5/tab5.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+@Component({
+  selector: 'app-tab5',
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss']
+})
+
+export class Tab5Page {
+
+  constructor() {}
+  
+}
