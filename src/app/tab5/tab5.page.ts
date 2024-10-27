9eb6580a386fcb6ed4185b4be85c943c6ec22a5d
shamy <shamyrquishpet@gmail.com>
2024-10-26 23:04:03 -0500

    paso final

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Directory, Filesystem, Encoding } from '@capacitor/filesystem';
 @Component({
   selector: 'app-tab5',
   templateUrl: 'tab5.page.html',
@@ -8,6 +8,21 @@ import { Directory, Filesystem } from '@capacitor/filesystem';
 
 export class Tab5Page {
 
-  constructor() {}
+  text: string = '';
+  message: string = '';
+
+  async saveFile() {
+    try {
+      await Filesystem.writeFile({
+        path: 'mi_archivo.txt',
+        data: this.text,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      });
+      this.message = 'Archivo guardado exitosamente, puedes encontrarlo en tu gestor de archivos en /documentos';
+    } catch (error:any) {
+      this.message = 'Error al guardar el archivo: ' + error.message;
+    }
+  }
   
 }
